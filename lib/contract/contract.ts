16535a4ad706f92d39a5ba46f4980a96c74d1cf3
Liangyu-Zhou <17816872833@163.com>
2023-01-04 14:43:53 +0800

    fix install and build problem

--- lib/contract/contract.ts
+++ lib/contract/contract.ts
@@ -6,7 +6,6 @@ import { FSNChinese } from './FSNChinese';
 import abi from './FSN-Chinese.json';
 
 let chineseWithSigner: FSNChinese;
-let chineseConfig: {};
 if (
   process.env.NEXT_PUBLIC_TEST_ACCOUNT_7001_PRIVATEKEY &&
   process.env.NEXT_PUBLIC_TEST_FRC759_CHINESE_ADDR
@@ -26,13 +25,7 @@ if (
     process.env.NEXT_PUBLIC_TEST_FRC759_CHINESE_ADDR,
     signer
   );
-
-  chineseConfig = {
-    address: '0x86fbbb1254c39602a7b067d5ae7e5c2bdfd61a30',
-    abi
-  };
 } else {
   throw new Error('Base account not found');
 }
 export const ChineseWithSigner = chineseWithSigner;
-export const ChineseConfig = chineseConfig;
